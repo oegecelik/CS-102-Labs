924dcb0fe2c4e0c39b7dd5862eddc4e3bc44720f
Orhun Ege Çelik <77338444+oegecelik@users.noreply.github.com>
2024-04-09 13:00:49 +0300

    Delete CS 102 Labs directory

--- CS 102 Labs/Lab04/BusDisplayer.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame;
-
-public class BusDisplayer{
-    public static void main(String[] args) {
-        BusFrame bus = new BusFrame();
-        bus.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        bus.setVisible(true);
-    }
-}
